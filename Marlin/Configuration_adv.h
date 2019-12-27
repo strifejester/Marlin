01dcbcfcc9b5784922029b37868d5e758000b892
Justin Ellenbecker <strifejester@gmail.com>
2019-12-27 00:12:53 -0600

    Update Configuration_adv.h
    Changed XYZ Stepping to 256 from 16. Next will try to enable Hybrid Threshhold but I have had issues building when microsteps are at 256 and Hybrid is on. I believe this was from an old build envioronment and this branch is build from scratch on a fresh install of VS Code and Platform.io

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1882,7 +1882,7 @@
   #if AXIS_IS_TMC(X)
     #define X_CURRENT       760        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
-    #define X_MICROSTEPS     16    // 0..256
+    #define X_MICROSTEPS     256    // 0..256
     #define X_RSENSE          0.11
     #define X_CHAIN_POS      -1    // <=0 : Not chained. 1 : MCU MOSI connected. 2 : Next in chain, ...
   #endif
@@ -1898,7 +1898,7 @@
   #if AXIS_IS_TMC(Y)
     #define Y_CURRENT       760
     #define Y_CURRENT_HOME  Y_CURRENT
-    #define Y_MICROSTEPS     16
+    #define Y_MICROSTEPS     256
     #define Y_RSENSE          0.11
     #define Y_CHAIN_POS      -1
   #endif
@@ -1914,7 +1914,7 @@
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT       760
     #define Z_CURRENT_HOME  Z_CURRENT
-    #define Z_MICROSTEPS     16
+    #define Z_MICROSTEPS     256
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
   #endif
